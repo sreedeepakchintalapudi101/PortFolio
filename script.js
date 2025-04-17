4a7077d7f610b2f43d77a55170225e1e8a494265
Deepak <deepak.ch@algonox.com>
2025-04-17 14:54:50 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add interactivity (e.g., contact form message)
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thanks for reaching out! I'll get back to you soon. 😊");
+    this.reset();
+  });
+  
